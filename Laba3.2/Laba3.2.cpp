72db7b8841e923f924d79ba569743af799387ff9
AlinaGaz <alinagazizova576@gmail.com>
2022-12-26 07:03:17 +0300

    Добавьте файлы проекта.

---
+++ Laba3.2/Laba3.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    int a = 10;
+    int* ptrA = &a;
+    cout << "pointer: " << ptrA << " " << "value: " << a << endl;
+    *ptrA = 8;
+    cout << "value: " << a << endl;
+    int arr[5];
+    *(arr) = 5;
+    *(arr + 1) = 7;
+    *(arr + 2) = 8;
+    *(arr + 3) = 11;
+    *(arr + 4) = 23;
+    for (int i = 0; i < 5; i++) {
+        cout << "arr[" << i << "]" << *(arr + i) << endl;
+    }
+    int b = 5;
+    int* const ptrB = &b;
+    //ptrB = ptrA; //VS сразу выдаёт ошибку.
+    *ptrB = 71;
+    cout << "value b: " << b << endl;
+}
